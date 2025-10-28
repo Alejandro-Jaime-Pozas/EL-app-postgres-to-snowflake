13e21f6a2f9557fe32f94e64c8659e63b56282bd
Alex <ajaimepozas@gmail.com>
2025-10-28 12:34:17 -0600

    copied template for new dag version of spark etl process

---
+++ dags/spark_psql_to_snowflake.py
@@ -0,0 +1,30 @@
+from datetime import datetime, timedelta
+
+from python_code.main_ai import extract_and_load
+
+from airflow.sdk import dag, task
+
+
+default_args = {
+    'owner': 'Alex',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=5)
+}
+
+
+# just need a simple dag schedule to trigger the python code...which is an overwrite of the table for now (later implement insert on date cursor)
+@dag(
+    dag_id='postgres_employee_schema_to_snowflake',
+    start_date=datetime(2025, 10, 26),
+    schedule='0 6 * * *'  # tweak schedule for testing
+)
+def overwrite_tables_to_snowflake():
+
+    @task()
+    def extract_and_load_task():
+        return extract_and_load()
+
+    extract_and_load_task()
+
+
+trigger_overwrite_tables_to_snowflake = overwrite_tables_to_snowflake()
