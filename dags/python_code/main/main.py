58c8a0eb47c57279c1b62f1c1bf210f13296e7d9
Alex <ajaimepozas@gmail.com>
2025-11-05 10:59:55 -0600

    added but commented out code since creating errors for creating parquet files from psql

--- dags/python_code/main/main.py
+++ dags/python_code/main/main.py
@@ -4,12 +4,27 @@
     # use airflow hooks/operators instead of directly from postgres/snowflake connectors
 
 
+# import os
+# import pandas as pd
+# import pyarrow as pa
+# import pyarrow.dataset as ds
+# from sqlalchemy import create_engine, text
+
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 
 
+# Connections Airflow UI
 PG_CONN_ID='pg-local'  # not local but ok, change later
 SF_CONN_ID='sf-default'
+
+# Extract from postgres
+
+
+# # Load to S3
+# S3_URI = "s3://postgres-neon-db-employees/db-data/schemas/"  # partition path you like
+# ROWS_PER_FILE = 250_000
+
 
 # 1. extract all schemas from the db from information_schema
 # prob need a pg cursor i can reuse in other fns
@@ -20,6 +35,17 @@ def get_pg_cursor():
     cur = conn.cursor()
     # print('cursor successfully extracted: ', cur)
     return cur
+
+
+def get_sqlalchemy_conn_uri(conn_id: str) -> str:
+    """
+    Returns a SQLAlchemy-style connection URI from an Airflow connection.
+    """
+    from airflow.hooks.base import BaseHook
+
+    conn = BaseHook.get_connection(conn_id)
+    uri = conn.get_uri()
+    return uri
 
 
 # 2. for each schema, extract all table names for that schema using information_schema
@@ -47,6 +73,35 @@ def get_schemas():
 
 
 # 2.5 copy all psql tables into s3 bucket as parquet files, use <proj_name>/db-data/schemas/<schema>/<table> for bucket location
+# # pip install psycopg2-binary SQLAlchemy pandas pyarrow s3fs
+
+# engine = create_engine(PG_DSN)
+
+# # Use a consistent snapshot so the whole export sees one point in time
+# with engine.begin() as conn:
+#     conn.execute(text("SET TRANSACTION ISOLATION LEVEL REPEATABLE READ"))
+#     # Narrow/select columns explicitly if needed
+#     sql = text("SELECT * FROM public.my_table ORDER BY id")
+#     chunks = pd.read_sql(sql, conn, chunksize=50_000)
+
+#     # Set up Arrow S3 filesystem sink and write in multiple files
+#     fs = pa.fs.S3FileSystem()  # reads AWS creds from env/profile/role
+
+#     # We'll append to a dataset; Arrow rotates files for us.
+#     writer = ds.DatasetWriter(
+#         base_dir=S3_URI,
+#         schema=None,                   # infer from first batch
+#         filesystem=fs,
+#         format=ds.ParquetFileFormat(), # Parquet output
+#         max_rows_per_file=ROWS_PER_FILE
+#     )
+
+#     try:
+#         for df in chunks:
+#             table = pa.Table.from_pandas(df, preserve_index=False)
+#             writer.write_table(table)
+#     finally:
+#         writer.finish()
 
 
 
